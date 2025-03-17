1288e8e4ad9a40110c81d963617d0efec01d0a3b
Lucas Zanini da Silva <127700075+LucasZanini096@users.noreply.github.com>
2025-03-17 13:02:14 -0300

    Add files via upload

---
+++ FUNDATIONS_IOS_AULA04/Desafio2.swift
@@ -0,0 +1,20 @@
+//
+//  Desafio2.swift
+//  FUNDATIONS_IOS_AULA04
+//
+//  Created by Aluno Mack on 17/03/25.
+//
+
+import SwiftUI
+
+struct Desafio2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Desafio2_Previews: PreviewProvider {
+    static var previews: some View {
+        Desafio2()
+    }
+}
---
+++ FUNDATIONS_IOS_AULA04/FUNDATIONS_IOS_AULA04App.swift
@@ -0,0 +1,17 @@
+//
+//  FUNDATIONS_IOS_AULA04App.swift
+//  FUNDATIONS_IOS_AULA04
+//
+//  Created by Aluno Mack on 17/03/25.
+//
+
+import SwiftUI
+
+@main
+struct FUNDATIONS_IOS_AULA04App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
